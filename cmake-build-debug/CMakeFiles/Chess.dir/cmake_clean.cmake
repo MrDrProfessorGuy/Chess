272b6c12904a8834117fccbae90d55d5442324fc
MrDrprofessorGuy <guycohen801@gmail.com>
2021-05-16 18:42:37 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/chessSystem.c.o"
+  "Chess"
+  "Chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
