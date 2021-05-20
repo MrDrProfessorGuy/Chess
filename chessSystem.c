8663b691b346e86fb1dcf4269f2830cd44f9b91a
MrDrprofessorGuy <guycohen801@gmail.com>
2021-05-20 21:27:41 +0300

    half update...

--- chessSystem.c
+++ chessSystem.c
@@ -5,22 +5,93 @@
 #include <stdbool.h>
 
 /***** typedef *****/
-typedef const int PlayerId;// Key = player_id
-typedef struct player_data PlayerData;
+typedef int* PlayerId;// Key = player_id
+typedef struct player_data* PlayerData;
 
-typedef const char* GameId; // key = "player1_id"+"player2_id"
-typedef struct game_data GameData;
+typedef char* GameId; // key = "player1_id"+"player2_id"
+typedef struct game_data* GameData;
 
-typedef const int TournamentId;// keys = 1,2,3,....
-typedef struct tournament_data TournamentData;
+typedef int* TournamentId;// keys = 1,2,3,....
+typedef struct tournament_data* TournamentData;
 
 
 typedef const char* Location;
 
+/***************************************************************/
+/********************* Helper Functions *********************/
+static char* idToSting(int num);
+static int countDigits(int num);
+static bool tournamentIdIsValid(TournamentId id);
 
-/***** helper functions *****/
-static char* id_to_str(int num);
-static int count_digits(int num);
+/********************* Player functions *********************/
+static void freePlayerData(PlayerData data);
+static void frePlayerId(PlayerId id);
+static PlayerData copyPlayerData(PlayerData data);
+static PlayerId copyPlayerId(PlayerId id);
+static int comparePlayerId(PlayerId id1, PlayerId id2);
+
+static PlayerId createPlayerId(int player1_id);
+static PlayerData createPlayerData();
+static bool addPlayer(Map player_map, PlayerId id);
+
+/********************* Tournament functions *********************/
+static void freeTournamentData(TournamentData data);
+static void freTournamentId(TournamentId id);
+static TournamentData copyTournamentData(TournamentData data);
+static TournamentId copyTournamentId(TournamentId id);
+static int compareTournamentId(TournamentId* id1, TournamentId* id2);
+
+static TournamentId createTournamentId();
+static TournamentData createTournamentData();
+static bool hasTournamentEnded(ChessSystem chess, TournamentId id);
+
+
+/********************* Game functions *********************/
+static void freeGameData(GameData data);
+static void freeGameId(GameId id);
+static GameData copyGameData(GameData data);
+static GameId copyGameId(GameId id);
+static int compareGameId(GameId id1, GameId id2);
+
+static GameId createGameId(PlayerId player1_id, PlayerId player2_id);
+static GameData createGameData();
+
+
+
+
+struct chess_system_t{
+    Map tournament_map;
+    Map player_map;
+};
+
+struct player_data{
+    int num_of_games; // used to calculate averages when needed
+    int num_of_wins; // used to calculate Level
+    int num_of_loses; // used to calculate Level
+    int num_of_draws; // used to calculate Level
+    int total_play_time; // used to calculate average playtime
+};
+
+struct tournament_data{
+    Location location;
+    int max_games_per_player;
+    bool has_ended;
+    Map game_map;
+    int num_of_players; // printed in tournament statistics
+    int longest_game_time; // printed in tournament statistics
+    long total_game_time; // used for tournament statistics
+    PlayerId winner;
+};
+
+struct game_data{
+    int play_time;
+    PlayerId player1_id;
+    PlayerId player2_id;
+    Winner winner;
+};
+
+/*******************************************************/
+/********************* Functions *********************/
 
 /**
  * count_digits:
@@ -29,7 +100,7 @@ static int count_digits(int num);
  *
  * @return
  */
-static int count_digits(int num){
+static int countDigits(int num){
     int count = 0;
     do{
         num = num / 10;
@@ -45,7 +116,7 @@ static int count_digits(int num){
  *
  * @return
  */
-static char* id_to_str(int num){
+static char* idToSting(int num){
     int digits = count_digits(num);
     char* str = malloc(sizeof(char) * (digits + 1));// for null terminator
     if (str == NULL){
@@ -63,64 +134,68 @@ static char* id_to_str(int num){
     return str;
 }
 
-static GameId createGameId(int player1_id, int player2_id);
 
-static void freePlayerData(PlayerData data);
-static void frePlayerKey(PlayerId id);
-static PlayerData copyPlayerData(PlayerData data);
-static PlayerId copyPlayerId(PlayerId id);
-static int comparePlayerId(PlayerId id);
+/********************* Player functions *********************/
+/**
+ * tournamentIdIsValid:
+ *
+ * @param
+ *
+ * @return
+ */
+static bool playerIdIsValid(PlayerId id){
+    if (*id > 0){
+        return true;
+    }
+    return false;
+}
 
+/********************* Tournament functions *********************/
+/**
+ * tournamentIdIsValid:
+ *
+ * @param
+ *
+ * @return
+ */
+static bool tournamentIdIsValid(TournamentId id){
+    if (*id > 0){
+        return true;
+    }
+    return false;
+}
+static bool validPlayTime(int play_time){
+    if (play_time > 0){
+        return true;
+    }
+    return false;
+}
 
-static void freeTournamentData(TournamentData data);
-static void freTournamentKey(TournamentId id);
-static TournamentData copyTournamentData(TournamentData data);
-static TournamentId copyTournamentId(TournamentId id);
-static int compareTournamentId(TournamentId id);
+/**
+ * tournamentIdIsValid:
+ *
+ * @param
+ *
+ * @return
+ */
+static bool hasTournamentEnded(ChessSystem chess, TournamentId id){
+    TournamentData tournament_data = (TournamentData)mapGet(chess->tournament_map, &id);
+    bool tournament_ended = tournament_data->has_ended;
+    freeTournamentData(tournament_data);
+    return tournament_ended;
+}
 
-
-static void freeGameData(GameData data);
-static void freeGameKey(GameId id);
-static GameData copyGameData(GameData data);
-static GameId copyGameId(GameId id);
-static int compareGameId(GameId id);
-
-
-
-
-
-
-struct chess_system_t{
-    Map list_of_tournament;
-    Map player_list;
-};
-
-struct player_data{
-    int num_of_games; // used to calculate averages when needed
-    int num_of_wins; // used to calculate Level
-    int num_of_loses; // used to calculate Level
-    int num_of_draws; // used to calculate Level
-    int total_play_time; // used to calculate average playtime
-};
-
-struct tournament_data{
-    Location location;
-    int max_games_per_player;
-    bool has_ended;
-    Map game_list;
-    int num_of_players; // printed in tournament statistics
-    int longest_game_time; // printed in tournament statistics
-    long total_game_time; // used for tournament statistics
-    PlayerId winner;
-};
-
-struct game_data{
-    int play_time;
-    PlayerId player1_id;
-    PlayerId player2_id;
-    Winner winner;
-};
-
+/********************* Game functions *********************/
+/**
+ * tournamentIdIsValid:
+ *
+ * @param
+ *
+ * @return
+ */
+static bool gameExists(Map game_map, PlayerId player_id1, PlayerId player_id2){
+    GameId game_id = createGameId(player_id1, player_id2);
+}
 
 
 /**
@@ -129,7 +204,9 @@ struct game_data{
  * @return A new chess system in case of success, and NULL otherwise (e.g.
  *     in case of an allocation error)
  */
-ChessSystem chessCreate();
+ChessSystem chessCreate(){
+    printf("");
+}
 
 /**
  * chessDestroy: free a chess system, and all its contents, from
@@ -138,7 +215,9 @@ ChessSystem chessCreate();
  * @param chess - the chess system to free from memory. A NULL value is
  *     allowed, and in that case the function does nothing.
  */
-void chessDestroy(ChessSystem chess);
+void chessDestroy(ChessSystem chess){
+    printf("");
+}
 
 /**
  * chessAddTournament: add a new tournament to a chess system.
@@ -159,7 +238,9 @@ void chessDestroy(ChessSystem chess);
  *     CHESS_SUCCESS - if tournament was added successfully.
  */
 ChessResult chessAddTournament (ChessSystem chess, int tournament_id,
-                                int max_games_per_player, const char* tournament_location);
+                                int max_games_per_player, const char* tournament_location){
+    printf("");
+}
 
 /**
  * chessAddGame: add a new match to a chess tournament.
@@ -186,7 +267,66 @@ ChessResult chessAddTournament (ChessSystem chess, int tournament_id,
  */
 /// todo
 ChessResult chessAddGame(ChessSystem chess, int tournament_id, int first_player,
-                         int second_player, Winner winner, int play_time);
+                         int second_player, Winner winner, int play_time){
+    
+    if (chess == NULL){
+        return CHESS_NULL_ARGUMENT;
+    }
+    if (!tournamentIdIsValid(&tournament_id) || !playerIdIsValid(&first_player) ||
+        !playerIdIsValid(&second_player) || !comparePlayerId(&first_player, &second_player)){
+        return CHESS_INVALID_ID;
+    }
+    if (!mapContains(chess->tournament_map, &tournament_id)){
+        return CHESS_TOURNAMENT_NOT_EXIST;
+    }
+    TournamentData tournament_data = mapGet(chess->tournament_map, &tournament_id);
+    if (tournament_data->has_ended){
+        return CHESS_TOURNAMENT_ENDED;
+    }
+    
+    Map game_map = tournament_data->game_map;
+    assert(game_map);
+    GameId game_id = createGameId(&first_player, &second_player);
+    if (!game_id){
+        return CHESS_OUT_OF_MEMORY;
+    }
+    if (mapContains(game_map, game_id)){
+        freeGameId(game_id);
+        return CHESS_GAME_ALREADY_EXISTS;
+    }
+    
+    if (!validPlayTime(play_time)){
+        freeGameId(game_id);
+        return CHESS_INVALID_PLAY_TIME;
+    }
+    
+    Map player_map = chess->player_map;
+    PlayerData player1 = mapGet(player_map, &first_player);
+    if (player1 == NULL){
+        if (!addPlayer(player_map, &first_player)){
+            freeGameId(game_id);
+            return CHESS_OUT_OF_MEMORY;
+        }
+    }
+    PlayerData player2 = mapGet(player_map, &second_player);
+    if (player2 == NULL){
+        if (!addPlayer(player_map, &second_player)){
+            freeGameId(game_id);
+            mapRemove(player_map, &first_player);
+            return CHESS_OUT_OF_MEMORY;
+        }
+    }
+    
+    
+    if (player1->num_of_games >= tournament_data->max_games_per_player){
+    
+    }
+    
+    
+    
+    
+    freeTournamentData(tournament_data);
+}
 
 /**
  * chessRemoveTournament: removes the tournament and all the games played in it from the chess system
